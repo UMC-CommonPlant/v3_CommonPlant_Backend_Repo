2b307b978e4983b189c738fe912ff4fcfe5fa8ed
admin <admin@alm.com>
2026-08-07 09:09:55 +0900

    [Chore] fix test fail error

--- src/test/java/com/commonplant/garden/GardenApplicationTests.java
+++ src/test/java/com/commonplant/garden/GardenApplicationTests.java
@@ -4,7 +4,6 @@ import com.commonplant.garden.support.IntegrationTestSupport;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GardenApplicationTests extends IntegrationTestSupport {
 
 	@Test
